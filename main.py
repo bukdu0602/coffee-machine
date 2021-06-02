fd7088022593b459155d2fa28a354c2def8f29bb
Ryan Lim <ryan.hyun@gmail.com>
2021-06-01 23:01:19 -0700

    third

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ MENU = {
         "ingredients": {
             "water": 50,
             "coffee": 18,
+            "milk": 0,
         },
         "cost": 1.5,
     },
@@ -30,42 +31,36 @@ resources = {
     "coffee": 100,
 }
 
-money = 0
+money = []
 
 
 def start():
     user_answer = input("what would you like? (espresso/latte/cappuccino)")
     if user_answer == "report":
-        report()
-    if user_answer == "off":
+        return report()
+    elif user_answer == "off":
         return
-    if user_answer == "espresso":
-        espresso(user_answer)
-
-
-def espresso(user_answer):
-    check_resource(user_answer)
-    if pay(user_answer):
-        print("Here is your espresso Enjoy!")
-        start()
+    elif user_answer == "espresso" or user_answer == "latte" or user_answer == "cappuccino":
+        check_resource(user_answer)
+        pay(user_answer)
     else:
-        print("merong")
-
-
+        return()
 
 
 def report():
     print(f"water: {resources['water']}")
     print(f"Milk: {resources['milk']}")
     print(f"Coffee: {resources['coffee']}")
-    print(f"Money: {money}")
-    start()
+    print(f"Money: {sum(money)}")
+    return start()
 
 
 def check_resource(user_answer):
     for remain in resources:
-        for 
-check_resource()
+        if resources[remain] < MENU[user_answer]["ingredients"][remain]:
+            print(f"Sorry there is not enough {remain}")
+            return start()
+
 
 def pay(user_answer):
     print("Please insert coins.")
@@ -76,9 +71,19 @@ def pay(user_answer):
     total_received = (quarter * 0.25) + (dime * 0.1) + (nickel * 0.05) + (penny * 0.01)
     if total_received > MENU[user_answer]["cost"]:
         cal = round(total_received - MENU[user_answer]['cost'], 2)
+        money.append(MENU[user_answer]["cost"])
         print(f"Here is ${cal} in change. ")
-        return True
+        print(f"Here is your {user_answer} ☕ Enjoy!")
+        subtract(user_answer)
+        return start()
     else:
-        print("not enough money")
-        return
+        print("not enough money money refunded")
+        return start()
 
+
+def subtract(user_answer):
+    for ingredient in resources:
+        resources[ingredient] = resources[ingredient] - MENU[user_answer]["ingredients"][ingredient]
+
+
+start()
